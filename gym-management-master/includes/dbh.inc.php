b6a4faa9ec5ed44ae09f9cfd07261390fa1c8b66
jaydeepvalaking <164307674+jaydeepvalaking@users.noreply.github.com>
2024-03-21 18:09:14 +0530

    Add files via upload
    1.  copy this folder
    2.  paste this folder under  the Xampp/Htdocs

---
+++ gym-management-master/includes/dbh.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbServername = "localhost";
+$dbUsername = "jvala";
+$dbPassword = "807";
+$dbName = "loginsystem";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
